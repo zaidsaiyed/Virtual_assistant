468b631777aab790978c15278a7c2c521641a356
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2023-02-26 00:03:28 -0500

    Trying object oriented

--- responses.py
+++ responses.py
@@ -1,4 +1,10 @@
 import random
 
-def random_response(responses):
-    return random.choice(responses)
+class VirtualAssistant:
+    
+    def __init__(self):
+        self.greetings = ["Hello! How can I help you?", "Hi there! What can I do for you today?", "Greetings! What do you need assistance with?"]
+        self.goodbyes = ["Goodbye!", "See you later!", "Have a great day!"]
+        self.affirmations = ["Okay, I'll do that.", "Sure thing!", "No problem!", "I'm on it!"]
+        self.negations = ["I'm sorry, I can't do that.", "I'm afraid I can't help with that.", "That's outside my capabilities."]
+    
