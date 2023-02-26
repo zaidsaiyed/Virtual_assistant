f895394cf30842d2a9294f7a0ade891ee0051175
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2023-02-26 03:10:11 -0500

    Test

--- responses.py
+++ responses.py
@@ -1,6 +1,5 @@
 import random
 
-<<<<<<< HEAD
 class VirtualAssistant:
     
     def __init__(self):
@@ -9,8 +8,3 @@ class VirtualAssistant:
         self.affirmations = ["Okay, I'll do that.", "Sure thing!", "No problem!", "I'm on it!"]
         self.negations = ["I'm sorry, I can't do that.", "I'm afraid I can't help with that.", "That's outside my capabilities."]
         
-=======
-def random_response(responses):
-    
-    return random.choice(responses)
->>>>>>> a8337cb28792278b461cce9ff1bd084b4568d916
