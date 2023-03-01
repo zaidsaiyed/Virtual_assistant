c9a40a5272246d1b30a1070231deeaae68d2605e
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2023-03-01 02:52:35 -0500

    Created 3 functions

--- responses.py
+++ responses.py
@@ -7,4 +7,13 @@ class VirtualAssistant:
         self.goodbyes = ["Goodbye!", "See you later!", "Have a great day!"]
         self.affirmations = ["Okay, I'll do that.", "Sure thing!", "No problem!", "I'm on it!"]
         self.negations = ["I'm sorry, I can't do that.", "I'm afraid I can't help with that.", "That's outside my capabilities."]
-        
+        self.unclear = ["I'm sorry, I didn't understand that.", "I'm not sure what you mean.", "I'm not sure I understand."]
+    
+    def greet(self):
+        return random.choice(self.greetings)
+    
+    def goodbye(self):
+        return random.choice(self.goodbyes)
+
+    def affirm(self):
+        return random.choice(self.affirmations)
